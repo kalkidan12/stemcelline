8c5ddc37c1d8ee039a638259e3fe40b32d46fa78
kalkidan12 <kalget12@gmail.com>
2022-12-30 23:27:35 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import "./style.css";
 const Footer = () => {
 	return (
 		<footer className="p-4 bg-white sm:p-6 rounded-t-md dark:bg-slate-900">
-			<div className="md:flex md:justify-evenly">
-				<div className="map-container-2 w-full">
+			<div className="sm:block md:flex justify-center">
+				<div className="flex justify-center sm:mb-4 map-container-2 w-full mx-auto">
 					<iframe
 						src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed"
 						className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
@@ -13,10 +13,27 @@ const Footer = () => {
 						allowfullscreen
 					></iframe>
 				</div>
-				<div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+				<div className="my-4 md:ml-4 grid grid-cols-3 gap-8 sm:gap-6 sm:grid-cols-3">
 					<div>
 						<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
 							Use full Links
+						</h2>
+						<ul className="text-gray-600 dark:text-gray-400">
+							<li className="mb-4">
+								<a href="#" className="hover:underline ">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:underline">
+									About
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+							Services
 						</h2>
 						<ul className="text-gray-600 dark:text-gray-400">
 							<li className="mb-4">
@@ -51,15 +68,15 @@ const Footer = () => {
 				</div>
 			</div>
 			<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-			<div className="sm:flex sm:items-center sm:justify-between">
-				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+			<div className="sm:flex sm:items-center sm:justify-center">
+				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mr-2">
 					© 2023{" "}
 					<a href="#" className="hover:underline">
 						Stemcelline™
 					</a>
 					. All Rights Reserved.
 				</span>
-				<div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+				<div className="flex space-x-6 sm:justify-center">
 					<a
 						href="#"
 						className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
